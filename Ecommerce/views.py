bbe29e7d6396bab1f4687cf2806ac4f59e4ace71
Basil Brian <brianshiaba33@gmail.com>
2024-06-14 11:05:04 +0300

    second commit

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from itertools import product
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from . models import Product
+from . models import  Customer, Product
 
 # Create your views here.
 def product_list(request):
@@ -9,3 +10,26 @@ def product_list(request):
         'products': products
     }
     return render(request, 'Ecommerce/product_list.html', context)
+
+
+def product_detail(request, pk):
+    Product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'Ecommerce/product_detail', context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers': customers
+    }
+    return render(request, 'Ecommerce/customer_list', context)
+
+def customer_detail(request, pk):
+    customer = get_object_or_404
+    context ={
+        'customers': customer
+    }
+    return render(request, 'Ecommerce/customer_detail', context )
+
